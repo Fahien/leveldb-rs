cb649e16112601f0ab8ec83bb30bded74cbe9503
Antonio Caggiano <info@antoniocaggiano.eu>
2021-09-17 17:08:19 +0200

    Wrap around structs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,61 @@
-use std::ffi::CString;
+use std::{ffi::CString, ptr::NonNull};
 
 use leveldb_sys::*;
 
-fn open(path: &str) -> *mut leveldb_t {
-    let options = unsafe { leveldb_options_create() };
-    let value = 1;
-    unsafe { leveldb_options_set_create_if_missing(options, value) };
-    let name = CString::new(path).unwrap();
-    let mut errdata = core::ptr::null_mut();
-    let db = unsafe { leveldb_open(options, name.as_ptr(), &mut errdata) };
-    if db == core::ptr::null_mut() {
-        let errmsg = unsafe { CString::from_raw(errdata) };
-        panic!("{:?}", errmsg);
-    }
-
-    db
+struct LdbOptions {
+    handle: std::ptr::NonNull<leveldb_options_t>,
 }
 
-fn close(db: *mut leveldb_t) {
-    unsafe { leveldb_close(db) };
+impl LdbOptions {
+    fn new() -> Self {
+        let handle = unsafe { leveldb_options_create() };
+        unsafe { leveldb_options_set_create_if_missing(handle, 1) };
+        Self {
+            handle: NonNull::new(handle).unwrap(),
+        }
+    }
+}
+
+impl Drop for LdbOptions {
+    fn drop(&mut self) {
+        unsafe { leveldb_options_destroy(self.handle.as_ptr()) };
+    }
+}
+
+struct Ldb {
+    handle: std::ptr::NonNull<leveldb_t>,
+    path: CString,
+}
+
+impl Ldb {
+    fn new(path: &str, options: &LdbOptions) -> Self {
+        let path = CString::new(path).unwrap();
+        let mut errdata = core::ptr::null_mut();
+        let db = unsafe { leveldb_open(options.handle.as_ptr(), path.as_ptr(), &mut errdata) };
+        if db == core::ptr::null_mut() {
+            let errmsg = unsafe { CString::from_raw(errdata) };
+            panic!("{:?}", errmsg);
+        }
+        println!("LevelDB {:?} opened", path);
+
+        Self {
+            handle: NonNull::new(db).unwrap(),
+            path,
+        }
+    }
+
+    fn open_or_create(path: &str) -> Self {
+        Self::new(path, &LdbOptions::new())
+    }
+}
+
+impl Drop for Ldb {
+    fn drop(&mut self) {
+        unsafe { leveldb_close(self.handle.as_ptr()) };
+        println!("LevelDB {:?} closed", self.path);
+    }
 }
 
 fn main() {
-    let db = open("src/data.db");
-    close(db);
+    Ldb::open_or_create("src/data.db");
 }
